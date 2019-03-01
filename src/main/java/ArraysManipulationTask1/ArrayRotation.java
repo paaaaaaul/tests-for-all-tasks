022de1db3b669f914c0f05113c4412b82f8259f7
Paul L <paaaaaaaaul13@gmail.com>
2019-03-01 19:01:49 +0200

    not good begining

--- src/main/java/ArraysManipulationTask1/ArrayRotation.java
+++ src/main/java/ArraysManipulationTask1/ArrayRotation.java
@@ -16,38 +16,42 @@ Output
 
 package ArraysManipulationTask1;
 
+import java.util.Scanner;
+
 public class ArrayRotation {
-    public int moveToEnd(int arr[], int n, int k) {
+    public static int[] moveToEnd(int arr[], int n, int k) {
         for (int i = 0; i < k; i++) {           // к примеру, к = 2, и массиве {1, 2, 3, 4, 5, 6}
             int x = arr[0];                     // в этот массив запишем
             for (int j = 0; j < n - 1; ++j) {   // j - индекс массива, по которому итерируемся
                 arr[j] = arr[j + 1];             // n - индекс, к которому перемещаем элемент
             }
-             arr[n - 1] = x;                     // на каждом проходе
+            arr[n - 1] = x;                     // на каждом проходе
         }
-        return 0;
+        return arr;
     }
 
-//    public static void main(String[] args) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//        Scanner kScanner = new Scanner(System.in);
-//
-//        int size = scanner.nextInt(); // задаем размер массива
-//        int elToMove = kScanner.nextInt(); // задаем кол-во элементов, которые нуэно переместить
-//
-//        int arr[] = new int[size]; // массив, кладем size, как размер массива
-//        int n = arr.length; // длина массива
-//
-//        for (int i = 0; i < size; i++) { // в цикле заполняем массив
-//            arr[i] = scanner.nextInt();
-//        }
-//
-//        moveToEnd(arr, n, elToMove); //передаем параметры в метод, который будет переносить элементы массива
-//
-//        for (int i = 0; i < n; ++i) {
-//            System.out.print(arr[i] + " "); //выводим результат в консоль
-//        }
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Scanner kScanner = new Scanner(System.in);
+
+        int size = scanner.nextInt(); // задаем размер массива
+        int elToMove = kScanner.nextInt(); // задаем кол-во элементов, которые нуэно переместить
+
+        int arr[] = new int[size]; // массив, кладем size, как размер массива
+        int n = arr.length; // длина массива
+
+        for (int i = 0; i < size; i++) { // в цикле заполняем массив
+            arr[i] = scanner.nextInt();
+        }
+
+        moveToEnd(arr, n, elToMove); //передаем параметры в метод, который будет переносить элементы массива
+
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " "); //выводим результат в консоль
+        }
 
     }
+}
+
 
--- src/test/java/ArraysManipulationTask1/ArrayRotationTest.java
+++ src/test/java/ArraysManipulationTask1/ArrayRotationTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
@@ -13,24 +12,13 @@ public class ArrayRotationTest {
 
     @Test
     public void testArrayRotation() {
-        Scanner scanner = new Scanner(System.in);
-        Scanner kScanner = new Scanner(System.in);
 
-        int size = scanner.nextInt(); // задаем размер массива
-        int elToMove = kScanner.nextInt(); // задаем кол-во элементов, которые нуэно переместить
+//        ArrayRotation arrayRotationTest = new ArrayRotation();
+        int [] arr = {1, 4, 7, 9, 99, 5};
+        int [] arrExp = {1, 4, 9, 99, 5, 7};
 
-        int arr[] = new int[size]; // массив, кладем size, как размер массива
-        int n = arr.length; // длина массива
+        assertEquals(arrExp, ArrayRotation.moveToEnd(arr ,2,6));
 
-        for (int i = 0; i < size; i++) { // в цикле заполняем массив
-            arr[i] = scanner.nextInt();
-        }
-
-//        moveToEnd(arr, n, elToMove); //передаем параметры в метод, который будет переносить элементы массива
-
-        for (int i = 0; i < n; ++i) {
-            System.out.print(arr[i] + " "); //выводим результат в консоль
-        }
     }
 
 }
